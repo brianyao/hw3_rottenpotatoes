96fbb07314c4064814682a07966b4b98ca70cf01
BrianYao <sc.xiong@qq.com>
2013-04-26 08:58:32 -0700

    TDD 26 April

--- Gemfile
+++ Gemfile
@@ -13,10 +13,16 @@ group :development, :test do
   gem 'capybara'
   gem 'launchy'
   gem 'rspec-rails'
+  gem 'ZenTest'
+  gem 'cucumber-rails'
+  gem 'cucumber-rails-training-wheels'
+
 end
 group :test do
   gem 'cucumber-rails'
   gem 'cucumber-rails-training-wheels'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 group :production do
 #  gem 'pg'
@@ -41,3 +47,6 @@ gem 'jquery-rails'
 
 # To use debugger
 gem 'haml'
+
+# The ruby-tmdb gem
+gem 'ruby-tmdb'
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,20 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  class Movie::InvalidKeyError < StandardError ; end
+
+  def self.api_key
+  	'e3d1bde3a478bc9f409b4f36668b986f'
+  end
+
+  def self.find_in_tmdb(string)
+  	Tmdb.api_key = self.api_key
+  	begin
+      TmdbMovie.find(:title => string)
+    rescue ArgumentError => tmdb_error
+    	raise Movie::InvalidKeyError, tmdb_error.message
+    end
+  end
+  # rest of file elided for brevity
 end
